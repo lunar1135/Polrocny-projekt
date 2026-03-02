5c45883457654b9662c092aa70c03fb19255073e
lunar1135 <165881214+lunar1135@users.noreply.github.com>
2026-03-02 15:43:29 +0100

    navbar

--- hono-client/src/App.vue
+++ hono-client/src/App.vue
@@ -44,21 +44,28 @@ async function addUser() {
 </script>
 
 <template>
-  <button @click="fetchUsers()">Get all users</button>
+  <div class="bg-primary w-full h-15 flex items-center justify-between ">
+    <div class="flex items-center ">
+      <img href="" src="/images/logo.png" alt="Voltix Logo" class="h-12 w-12 ml-4 mr-0.5 aspect-5">
+      <h1 class="text-text font-bold">Voltix</h1>
+    </div>
+    
+    <div class="flex items-center bg-white rounded border-2 border-secondary ">
+      🔍<input type="text" v-model="text" class="bg-white ml-auto mr-4 px-2 py-1 rounded" placeholder="Search...">
+      <button class="bg-secondary text-white px-4 py-1 rounded">search</button>
+    </div>
 
-  <ul>
-    <li v-for="(user, index) in users" @click="deleteUser(index)">
-      {{ user.toLowerCase() }}
-    </li>
-  </ul>
+    <div class="">
+      <button class="bg-secondary text-white px-4 py-1 rounded">
+        <img src="/images/default.webp" alt="profile picture" class="h-8 w-8 rounded-full">
+        <p>name</p>
+      </button>
+    </div>
 
-  <h1>{{ text }}</h1>
-
-  <div>
-    <input type="text" v-model="text" />
-    <button @click="addUser()">Add</button>
+    
+        
   </div>
 
 
-  
+
 </template>
---
+++ reports/report-3/report3.md
@@ -0,0 +1,10 @@
+# Report [2026-02-24]
+
+vybral som farby pre stranku a zacal som robim nav bar
+
+# Report [2026-03-01]
+
+urobil som logo a dorobil nav bar
+
+![alt text](../../hono-client/public/images/logo.png)
+
